63e49c1ab0cc0bc3b8ebc3783dec95ad82b91d09
Ryan Benson <benson.ryan@gmail.com>
2022-01-01 14:03:11 -0600

    Adds basic form for  name and ingredients list

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div id="nav">
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/about">About</router-link> |
+    <router-link to="/recipes/create">Create Recipe</router-link>
   </div>
   <router-view />
 </template>
--- src/router/index.js
+++ src/router/index.js
@@ -16,6 +16,17 @@ const routes = [
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About.vue"),
   },
+  {
+    path: "/recipes/create",
+    name: "Create",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(
+        /* webpackChunkName: "recipes-create" */ "../views/recipes/Create.vue"
+      ),
+  },
 ];
 
 const router = createRouter({
